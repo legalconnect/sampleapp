824fa1d137a9be79cb09f6a5719d287e3c39e100
seesi <seesi@accede.dev>
2024-04-30 20:39:15 +0000

    Adds Client to user context

--- src/UserContext.tsx
+++ src/UserContext.tsx
@@ -1,28 +1,35 @@
 import { User } from "oidc-client-ts";
 import { createContext, useContext, useEffect, useState } from "react";
 import { userManager } from "./AuthManager";
+import { ClientsService, LegalConnect_Common_Dtos_ClientDto } from "./services";
 
 export interface IUserContext{
-    user: User
+    user: User,
+    client: LegalConnect_Common_Dtos_ClientDto
 }
 export const UserContext = createContext<IUserContext>(null as any);
 
 export function UserContextProvider(props: any)
 {
     const [user, setUser] = useState(null as any);
+    const [client, setClient] = useState(null as any);
 
     useEffect(()=>{
-        userManager.getUser().then(response=> setUser(response));
+        userManager.getUser().then( async response=> {
+            setUser(response)
+            const res = await ClientsService.getApiV1ClientsMe()
+            setClient(res.result);
+        });
     },[]);
     // if(!user)
     //     return null;
-    console.log("User: ",user);
-    return <UserContext.Provider value={{user}}>
+    
+    return <UserContext.Provider value={{user,client}}>
         {props.children}
     </UserContext.Provider>
 }
 export function useUser(){
 
     var context =  useContext(UserContext)
-    return context.user;
+    return {user: context.user, client: context.client};
 }
